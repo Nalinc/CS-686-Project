30443fe687f21720d30499112417eec7f3f60850
nalinc <nalin.chhibber@gmail.com>
2017-12-19 01:05:40 -0500

    add figures

--- src/botLSTMTrain.py
+++ src/botLSTMTrain.py
@@ -19,12 +19,12 @@ vec_y=np.array(vec_y,dtype=np.float64)
 
 x_train,x_test, y_train,y_test = train_test_split(vec_x, vec_y, test_size=0.2, random_state=1)
 
-# model=Sequential()
-# model.add(LSTM(output_dim=300,input_shape=x_train.shape[1:],return_sequences=True, init='glorot_normal', inner_init='glorot_normal', activation='sigmoid'))
-# model.add(LSTM(output_dim=300,input_shape=x_train.shape[1:],return_sequences=True, init='glorot_normal', inner_init='glorot_normal', activation='sigmoid'))
-# model.add(LSTM(output_dim=300,input_shape=x_train.shape[1:],return_sequences=True, init='glorot_normal', inner_init='glorot_normal', activation='sigmoid'))
-# model.add(LSTM(output_dim=300,input_shape=x_train.shape[1:],return_sequences=True, init='glorot_normal', inner_init='glorot_normal', activation='sigmoid'))
-# model.compile(loss='cosine_proximity', optimizer='adam', metrics=['accuracy'])
+model=Sequential()
+model.add(LSTM(output_dim=300,input_shape=x_train.shape[1:],return_sequences=True, init='glorot_normal', inner_init='glorot_normal', activation='sigmoid'))
+model.add(LSTM(output_dim=300,input_shape=x_train.shape[1:],return_sequences=True, init='glorot_normal', inner_init='glorot_normal', activation='sigmoid'))
+model.add(LSTM(output_dim=300,input_shape=x_train.shape[1:],return_sequences=True, init='glorot_normal', inner_init='glorot_normal', activation='sigmoid'))
+model.add(LSTM(output_dim=300,input_shape=x_train.shape[1:],return_sequences=True, init='glorot_normal', inner_init='glorot_normal', activation='sigmoid'))
+model.compile(loss='cosine_proximity', optimizer='adam', metrics=['accuracy'])
 
 # model.fit(x_train, y_train, nb_epoch=500,validation_data=(x_test, y_test))
 # model.save('LSTM500.h5');
@@ -47,16 +47,20 @@ x_train,x_test, y_train,y_test = train_test_split(vec_x, vec_y, test_size=0.2, r
 # model.fit(x_train, y_train, nb_epoch=500,validation_data=(x_test, y_test))
 # model.save('LSTM5000.h5');
 
-model = load_model('LSTM4500.h5')
+# model = load_model('LSTM4500.h5')
 
-history = model.fit(x_train, y_train, nb_epoch=500,validation_data=(x_test, y_test))
-model.save('LSTM5000_new.h5')
+history = model.fit(x_train, y_train, nb_epoch=5000,validation_data=(x_test, y_test))
+
+# with open('history.pickle','wb') as f:
+#     pickle.dump(history,f)
+
+model.save('LSTM5000_bulk.h5')
 
 font = {'family' : 'normal',
         'weight' : 'bold',
-        'size'   : 22}
+        'size'   : 16}
 
-matplotlib.rc('font', **font)
+plt.rc('font', **font)
 print(history.history.keys())
 
 plt.plot(history.history['acc'])
